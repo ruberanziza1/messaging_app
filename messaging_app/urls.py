76b83f572e74773a4af0e710cf551b4fb376991d
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-07-27 21:32:15 +0200

    Add files via upload

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,34 +1,20 @@
-"""
-URL configuration for messaging_app project.
+# project/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from chats.auth import TokenObtainPairView, TokenRefreshView
-from django.http import JsonResponse
-
-def home(request):
-    return JsonResponse({"message": "Welcome to Messaging App API 👋"})
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('', home),
     path('admin/', admin.site.urls),
+    
+    # Your app API routes
     path('api/', include('chats.urls')),
     path('api-auth/', include('rest_framework.urls')),
 
+    # Simple JWT auth endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/chats/', include('chats.urls')),
 ]
